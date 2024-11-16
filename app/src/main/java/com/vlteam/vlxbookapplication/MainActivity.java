15a1b597584d60b7e56fff338547bc1366b5f912
BuildTools <unconfigured@null.spigotmc.org>
2024-11-16 23:05:07 +0700

    test event

--- app/src/main/java/com/vlteam/vlxbookapplication/MainActivity.java
+++ app/src/main/java/com/vlteam/vlxbookapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.vlteam.vlxbookapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button openInfoButton = findViewById(R.id.openinfo_btn);
+
+        openInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InfoPage.class);
+                startActivity(intent);
+            }
         });
     }
 }
